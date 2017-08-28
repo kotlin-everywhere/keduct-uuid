acbf1edc5976d8631ce9c02f230d5692ec066d84
Park Sung Min <vamf12@gmail.com>
2017-08-28 15:41:32 +0900

    keuson decoder 추가

--- keduct-uuid-java/src/main/kotlin/com/minek/kotlin/everywhere/keduct/uuid/uuid.kt
+++ keduct-uuid-java/src/main/kotlin/com/minek/kotlin/everywhere/keduct/uuid/uuid.kt
@@ -1,12 +1,17 @@
 package com.minek.kotlin.everywhere.keduct.uuid
 
+import com.minek.kotlin.everywehre.keuson.decode.Decoder
+import com.minek.kotlin.everywehre.keuson.decode.Decoders
 import com.minek.kotlin.everywhere.kelibs.result.Err
 import com.minek.kotlin.everywhere.kelibs.result.Ok
 import com.minek.kotlin.everywhere.kelibs.result.Result
+import com.minek.kotlin.everywhere.kelibs.result.andThen
 import java.util.*
 
 data class Uuid(private val uuid: UUID) {
     companion object {
+        val decoder: Decoder<Uuid> = { Decoders.string(it).andThen { fromString(it) } }
+
         fun fromString(string: String): Result<String, Uuid> {
             return try {
                 Ok(Uuid(UUID.fromString(string)))
--- keduct-uuid-java/src/test/kotlin/com/minek/kotlin/everywhere/keduct/testUuid.kt
+++ keduct-uuid-java/src/test/kotlin/com/minek/kotlin/everywhere/keduct/testUuid.kt
@@ -1,5 +1,6 @@
 package com.minek.kotlin.everywhere.keduct
 
+import com.minek.kotlin.everywehre.keuson.decode.decodeString
 import com.minek.kotlin.everywhere.keduct.uuid.Uuid
 import com.minek.kotlin.everywhere.kelibs.result.err
 import com.minek.kotlin.everywhere.kelibs.result.ok
@@ -27,6 +28,17 @@ class TestUuid {
     fun testRandomUuid() {
         val uuid = Uuid.randomUuid()
         Assert.assertEquals(uuid, Uuid(UUID.fromString(uuid.toString())))
+    }
+
+    @Test
+    fun testDecoder() {
+        Assert.assertEquals(err("Expecting a String but instead got: null"), decodeString(Uuid.decoder, "null"))
+        Assert.assertEquals(err("Expecting a String but instead got: true"), decodeString(Uuid.decoder, "true"))
+        Assert.assertEquals(err("Expecting a String but instead got: 42"), decodeString(Uuid.decoder, "42"))
+        Assert.assertEquals(err("Expecting a String but instead got: 3.14"), decodeString(Uuid.decoder, "3.14"))
+        Assert.assertEquals(err("Invalid UUID string: hello"), decodeString(Uuid.decoder, "\"hello\""))
+        Assert.assertEquals(err("Expecting a String but instead got: {\"hello\":42}"), decodeString(Uuid.decoder, "{ \"hello\": 42 }"))
+        Assert.assertEquals(Uuid.fromString("87eb50f6-8bb7-11e7-814c-5b8cec49619a"), decodeString(Uuid.decoder, "87eb50f6-8bb7-11e7-814c-5b8cec49619a"))
     }
 }
 
